4f9e3cda1968dd819ff569278923458e059ab373
bloodshot <jdroque@gmail.com>
2014-08-26 20:16:20 -0400

    Fixed handshake issues when switching servers.

--- proxy/src/main/java/net/md_5/bungee/UserConnection.java
+++ proxy/src/main/java/net/md_5/bungee/UserConnection.java
@@ -34,6 +34,7 @@ import net.md_5.bungee.api.tab.TabListHandler;
 import net.md_5.bungee.chat.ComponentSerializer;
 import net.md_5.bungee.connection.InitialHandler;
 import net.md_5.bungee.entitymap.EntityMap;
+import net.md_5.bungee.forge.ForgeClientHandshakeState;
 import net.md_5.bungee.forge.IForgeClientData;
 import net.md_5.bungee.netty.ChannelWrapper;
 import net.md_5.bungee.netty.HandlerBoss;
@@ -114,6 +115,10 @@ public final class UserConnection implements ProxiedPlayer
     @Getter
     @Setter
     private ServerConnection serverConnection;
+    @NonNull
+    @Getter
+    @Setter
+    private ForgeClientHandshakeState state = ForgeClientHandshakeState.START;
     /*========================================================================*/
     private final Unsafe unsafe = new Unsafe()
     {
